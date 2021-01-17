689df848d6ea71ebbcfa59dfe6fe6061d3a42b88
Kevin <kevinjoseph97@gmail.com>
2021-01-17 16:29:12 -0500

    commit tree ee444c75fb613ca23b459dc5a60ce299a273388b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(number)
+    i = 2
+    if number > 1 
+        all_num = (i..number -1).to_a
+        all_num.none? do |num|
+            number % num == 0
+        end
+    else false
+    end
+end
